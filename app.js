99c51ed39e3f0a22d3b2029cb7f62a851387b847
sharonV33 <sharonveldman33@gmail.com>
2021-03-18 12:56:55 +0100

    add static survey questions remove course files save data from test survey to json file

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
+//express setup
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 
+//bodyParser setup
+const bodyParser = require('body-parser')
+// const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+//ejs setup
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(express.static('public'))
+const fs = require('fs')
 
 const port = 3000;
-
-
 
 app.get('/', function(req, res) {
     res.render('pages/index')
 })
 
+app.post('/result', urlencodedParser, function (req, res) {
+
+    const data = {
+
+    }
+    fs.writeFile(`${req.body.name}`, JSON.stringify(data), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    })
+
+    res.render('pages/result', {
+        name: req.body.name,
+        age: req.body.age
+    })
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
-});
+})
