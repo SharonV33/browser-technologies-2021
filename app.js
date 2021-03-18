a6061658d68d20ec52a6cdadb86e52e71c238002
sharonV33 <sharonveldman33@gmail.com>
2021-03-18 10:50:25 +0100

    get started on final project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+const port = 3000;
+
+
+
+app.get('/', function(req, res) {
+    res.render('pages/index')
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
