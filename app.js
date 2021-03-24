17f4e47b559b92d9ce240ae0ac4bd6b0cb5708cc
sharonV33 <sharonveldman33@gmail.com>
2021-03-24 11:38:57 +0100

    place data in a nicely formatted json file

--- app.js
+++ app.js
@@ -15,25 +15,24 @@ app.set('views', 'views')
 //set up fileSystem
 const fs = require('fs')
 
+let userData = []
+
 const port = 3000;
 
 app.get('/', function(req, res) {
     res.render('pages/index')
 })
 
-let userData = []
-
 //save data from student info and redirect to WN
 app.post('/WN', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.body.studentID}.json`
-    const data = {
-        studentInfo: {
+    const studentInfo = {
+            subject: 'studentInfo',
             name: req.body.name,
             studentID: req.body.studentID
-        },
     }
 
-    userData.push(data)
+    userData.push(studentInfo)
     if (fs.existsSync(path)) {
         console.log(`${req.body.name} has already done the survey`)
 
@@ -42,14 +41,10 @@ app.post('/WN', urlencodedParser, function (req, res) {
     }
     else {
         //write new entry
-        fs.writeFile(path, JSON.stringify(data), (err) => {
-            if (err) {
-                throw err;
-            }
+        fs.writeFile(path, JSON.stringify(studentInfo, null, 2), () => {
             console.log("JSON data is saved.")
         })
     }
-
     res.render('pages/WN-survey', {
         name: req.body.name,
         studentID: req.body.studentID
@@ -63,21 +58,18 @@ app.post('/WAFS/:id', urlencodedParser, function (req, res) {
     const currentData = JSON.parse(currentFile)
     console.log(currentData)
 
-    const data = {
-        weeklyNerd: {
+    const weeklyNerd = {
+            subject: 'weekly ',
             teacher: req.body.WNteacher,
             week: req.body.WNweek,
             material: req.body.WNmaterial,
             explanation: req.body.WNexplanation,
             insight: req.body.WNinsight
         }
-    }
-    userData.push(data)
+    userData.push(weeklyNerd)
     console.log(JSON.stringify(userData))
-    fs.writeFile(path, JSON.stringify(userData), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -90,19 +82,18 @@ app.post('/WAFS/:id', urlencodedParser, function (req, res) {
 //save data from WAFS and redirect to CSS
 app.post('/CSS/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        WAFS: {
+    const WAFS = {
+            subject: 'Web apps from scratch',
             teacher: req.body.WAFSteacher,
             week: req.body.WAFSweek,
             material: req.body.WAFSmaterial,
             explanation: req.body.WAFSexplanation,
             insight: req.body.WAFSinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(WAFS)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -114,19 +105,18 @@ app.post('/CSS/:id', urlencodedParser, function (req, res) {
 //save data from CSS and redirect to PWA
 app.post('/PWA/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        CSSTTR: {
+    const CSSTTR = {
+            subject: 'CSS to the rescue',
             teacher: req.body.CSSteacher,
             week: req.body.CSSweek,
             material: req.body.CSSmaterial,
             explanation: req.body.CSSexplanation,
             insight: req.body.CSSinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(CSSTTR)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -138,19 +128,18 @@ app.post('/PWA/:id', urlencodedParser, function (req, res) {
 //save data from PWA and redirect to BT
 app.post('/BT/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        PWA: {
+    const PWA = {
+            subject: 'Progressive web apps',
             teacher: req.body.PWAteacher,
             week: req.body.PWAweek,
             material: req.body.PWAmaterial,
             explanation: req.body.PWAexplanation,
             insight: req.body.PWAinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(PWA)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -162,19 +151,18 @@ app.post('/BT/:id', urlencodedParser, function (req, res) {
 //save data from BT and redirect to RTW
 app.post('/RTW/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        BT: {
+    const BT = {
+            subject: 'Browser technologies',
             teacher: req.body.BTteacher,
             week: req.body.BTweek,
             material: req.body.BTmaterial,
             explanation: req.body.BTexplanation,
             insight: req.body.BTinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(BT)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -186,19 +174,18 @@ app.post('/RTW/:id', urlencodedParser, function (req, res) {
 //save data from RTW and redirect to HDC
 app.post('/HCD/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        RTW: {
+    const RTW = {
+            subject: 'Real time web',
             teacher: req.body.RTWteacher,
             week: req.body.RTWweek,
             material: req.body.RTWmaterial,
             explanation: req.body.RTWexplanation,
             insight: req.body.RTWinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(RTW)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
@@ -210,19 +197,18 @@ app.post('/HCD/:id', urlencodedParser, function (req, res) {
 //save data from RTW and redirect to thank you page
 app.post('/result/:id', urlencodedParser, function (req, res) {
     const path = `./public/entries/${req.params.id}.json`
-    const data = {
-        HCD: {
+    const HCD = {
+            subject: 'Human centered design',
             teacher: req.body.HCDteacher,
             week: req.body.HCDweek,
             material: req.body.HCDmaterial,
             explanation: req.body.HCDexplanation,
             insight: req.body.HCDinsight
-        },
     }
-    fs.appendFile(path, JSON.stringify(data), (err) => {
-        if (err) {
-            throw err;
-        }
+
+    userData.push(HCD)
+
+    fs.writeFile(path, JSON.stringify(userData, null, 2), () => {
         console.log("JSON data is saved.")
     })
 
